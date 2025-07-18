21a96f5eac4d58fadd771f37e76e31c5369bad76
Tirth <tirthacodes@gmail.com>
2025-07-18 21:52:59 +0545

    Required and MaxLength data annotation using System.ComponenetModel.DataAnnotations;

--- MagicVilla_VillaApi/Models/DTO/VillaDTO.cs
+++ MagicVilla_VillaApi/Models/DTO/VillaDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace MagicVilla_VillaApi.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaApi.Models.DTO
 {
     public class VillaDTO
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
     }
 }
