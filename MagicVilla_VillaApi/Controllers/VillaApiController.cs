ffe940cbad699d88a0d8d29700d1098a6f833747
Tirth <tirthacodes@gmail.com>
2025-07-18 19:22:44 +0545

    using DTO in controller without exposing whole villa details

--- MagicVilla_VillaApi/Controllers/VillaApiController.cs
+++ MagicVilla_VillaApi/Controllers/VillaApiController.cs
@@ -1,4 +1,5 @@
 ﻿using MagicVilla_VillaApi.Models;
+using MagicVilla_VillaApi.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaApi.Controllers
@@ -8,12 +9,12 @@ namespace MagicVilla_VillaApi.Controllers
     public class VillaApiController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<Villa>
+            return new List<VillaDTO>
             {
-                new Villa { Id = 1, Name="Pool View"},
-                new Villa { Id = 2, Name="Beach View"}
+                new VillaDTO { Id = 1, Name="Pool View"},
+                new VillaDTO { Id = 2, Name="Beach View"}
             };
         }
 
