bd8854aa940b27483505cd5041ca6a7bb0bddb01
Tirth <tirthacodes@gmail.com>
2025-07-18 19:17:58 +0545

    using Route, ApiController, and HttpGet with IEnumerable returning function

--- MagicVilla_VillaApi/Controllers/VillaApiController.cs
+++ MagicVilla_VillaApi/Controllers/VillaApiController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_VillaApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaApi.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class VillaApiController : ControllerBase
     {
-
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1, Name="Pool View"},
+                new Villa { Id = 2, Name="Beach View"}
+            };
+        }
 
     }
 }
