060dbaec1d8ea473055604de3b478477e0471902
Tirth <tirthacodes@gmail.com>
2025-07-18 22:07:50 +0545

    Custom Validation to ModelState plus FirstOrDefault

--- MagicVilla_VillaApi/Controllers/VillaApiController.cs
+++ MagicVilla_VillaApi/Controllers/VillaApiController.cs
@@ -18,7 +18,7 @@ namespace MagicVilla_VillaApi.Controllers
         }
 
         [HttpGet("{id:int}", Name ="GetVillaByID")]
-        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         //[ProducesResponseType(200, Type= typeof(VillaDTO))]
@@ -40,8 +40,17 @@ namespace MagicVilla_VillaApi.Controllers
 
 
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
         {
+            if(VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Villa Already Exists!");
+                return BadRequest(ModelState);
+            }
+
             if(villaDTO == null)
             {
                 return BadRequest(villaDTO);
