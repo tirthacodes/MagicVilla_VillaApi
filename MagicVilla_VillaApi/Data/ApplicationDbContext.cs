63a888a832ce0c3c5656af83a71a27a313744dec
Tirth <tirthacodes@gmail.com>
2025-07-21 13:28:34 +0545

    migration and update database

--- MagicVilla_VillaApi/Data/ApplicationDbContext.cs
+++ MagicVilla_VillaApi/Data/ApplicationDbContext.cs
@@ -5,6 +5,13 @@ namespace MagicVilla_VillaApi.Data
 {
     public class ApplicationDbContext : DbContext
     {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
+        
+        }
+
+
+
         public DbSet<Villa> Villas { get; set; }
     }
 }
