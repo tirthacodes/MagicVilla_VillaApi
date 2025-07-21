4cd519b629e5eabc0cbe1e3162eb4e10965a9c46
Tirth <tirthacodes@gmail.com>
2025-07-21 13:55:47 +0545

    seeded data OnModelCreating modelBuilder

--- MagicVilla_VillaApi/Data/ApplicationDbContext.cs
+++ MagicVilla_VillaApi/Data/ApplicationDbContext.cs
@@ -13,5 +13,38 @@ namespace MagicVilla_VillaApi.Data
 
 
         public DbSet<Villa> Villas { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                    new Villa()
+                    {
+                        Id = 1,
+                        Name = "Ronaldo",
+                        Details = "lorem",
+                        ImageUrl = "https://media.architecturaldigest.com/photos/64471ebe071ada485c0ff168/2:1/w_1280%2Cc_limit/600ClintonPlace-mls-3.jpg",
+                        Occupancy = 5,
+                        Rate = 200,
+                        Sqft = 550,
+                        Amenity = "",
+                        CreatedDate = DateTime.Now
+                    },
+
+                    new Villa()
+                    {
+                        Id = 2,
+                        Name = "Suii",
+                        Details = "england",
+                        ImageUrl = "https://media.architecturaldigest.com/photos/64471ebe071ada485c0ff168/2:1/w_1280%2Cc_limit/600ClintonPlace-mls-3.jpg",
+                        Occupancy = 5,
+                        Rate = 400,
+                        Sqft = 750,
+                        Amenity = "",
+                        CreatedDate= DateTime.Now
+                    }
+                );
+        }
     }
 }
---
+++ MagicVilla_VillaApi/Migrations/20250721080507_SeedVillaTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagicVilla_VillaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenity", "CreatedDate", "Details", "ImageUrl", "Name", "Occupancy", "Rate", "Sqft", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, "", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "lorem", "https://media.architecturaldigest.com/photos/64471ebe071ada485c0ff168/2:1/w_1280%2Cc_limit/600ClintonPlace-mls-3.jpg", "Ronaldo", 5, 200.0, 550, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, "", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "england", "https://media.architecturaldigest.com/photos/64471ebe071ada485c0ff168/2:1/w_1280%2Cc_limit/600ClintonPlace-mls-3.jpg", "Suii", 5, 400.0, 750, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
