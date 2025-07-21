d86e5bb8318d1dccf3e030c35357f40e0136e3e8
Tirth <tirthacodes@gmail.com>
2025-07-21 13:36:12 +0545

    minor change typo corrected

---
+++ MagicVilla_VillaApi/Migrations/20250721074611_AddVillaTable1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_VillaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVillaTable1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ MagicVilla_VillaApi/Migrations/20250721074816_InitalMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_VillaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitalMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
